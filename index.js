1b9fda19debafbd2d8b80ae1a6b979eec6d33492
AShish Patel <ashishpatel287680@gmail.com>
2024-06-20 22:41:01 +0530

    Added common error handler

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ dbConnection()
 const port = process.env.PORT ?? 3000
 const app = express()
 
+
 app.get('/', (req, res, next) => {
     res.send("hello world!")
+})
+
+//common Error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({ status: err.status, message: err.message || "something went wrong!" })
 })
 
 app.listen(port, () => {
