a5677ce5207163b955c2cdf67deb5dd660e578ec
khushboo <khushboodesai2609@gmail.com>
2024-06-21 13:41:03 +0530

    user schema

---
+++ src/api/auth/auth.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose;
+
+
+//created model(Schema) for the User collection in database
+const UserSchema = new Schema({
+    //fields to be added in collection
+    uid: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//exporting the model module
+module.exports = mongoose.model('users', UserSchema)
