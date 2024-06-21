6b5f64f8f17a01877c767dc03a0511c42b15b0e2
AShish Patel <ashishpatel287680@gmail.com>
2024-06-21 22:37:47 +0530

    Added timestamps to the auth model

--- src/api/auth/auth.model.js
+++ src/api/auth/auth.model.js
@@ -3,29 +3,34 @@ const { Schema } = mongoose;
 
 
 //created model(Schema) for the User collection in database
-const UserSchema = new Schema({
-    //fields to be added in collection
-    userName: {
-        type: String,
-        required: true
+const UserSchema = new Schema(
+    {
+        //fields to be added in collection
+        userName: {
+            type: String,
+            required: true
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
     },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
+    {
+        timestamps: true
     }
-});
+);
 
 //exporting the model module
 export const Users = mongoose.model('users', UserSchema)
